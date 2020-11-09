95a82b4b67fa29965f3ee5dc15f60ea8be663f48
Ilan Herbach <ilan.herbach@gmail.com>
2020-11-09 18:55:42 -0300

    Sequelize config almost done

--- back-end/services/usersService.js
+++ back-end/services/usersService.js
@@ -1,4 +1,5 @@
 const { usersModel } = require('../models');
+const { User } = require('../models');
 
 const getUserByEmail = async (email) => {
   try {
@@ -12,7 +13,7 @@ const getUserByEmail = async (email) => {
 
 const addUser = async (name, email, password, role) => {
   try {
-    const user = await usersModel.createUser(name, email, password, role);
+    const user = await User.create(name, email, password, role);
     return user;
   } catch (error) {
     throw new Error(error.message);
