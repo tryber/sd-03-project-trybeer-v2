c00a0311dc6a027e87ec4f6ea407b3aba3dc96c8
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-10 16:04:03 -0300

    Testando orderDetails be

--- back-end/services/orderDetails.js
+++ back-end/services/orderDetails.js
@@ -2,7 +2,7 @@ const { salesProducts, products } = require('../models');
 
 const getDetails = async (orderNumber) => {
   const orderDetails = await salesProducts.findAll({ where: { sale_id: orderNumber } });
-
+  console.log(orderDetails);
   const details = orderDetails.map(async ({ quantity, product_id: productId }) => {
     // console.log(await products.findByPk(productId));
     const { name, price } = await products.findByPk(productId);
