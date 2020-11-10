24f742ee59e91dcb0302d3c4f5cee1028371415c
Jafet Henrique <jafetguerra6@gmail.com>
2020-11-10 16:46:39 -0300

    backend dos requisitos 2 e 3 feitos

--- back-end/socket/index.js
+++ back-end/socket/index.js
@@ -6,8 +6,14 @@ const addStatusToOrder = (status) => {
 
 };
 
+const findOrder = (id, socket) => {
+  const order = orders.find((order) => order.id === id);
+  return socket.emit('Status', order);
+}
+
 const onConnection = (socket) => {
   socket.on('Status', (status) => addStatusToOrder(status));
+  socketIo.on('Status-id', (id) => findOrder(id, socket))
 };
 
 module.exports = (httpServer) => {
