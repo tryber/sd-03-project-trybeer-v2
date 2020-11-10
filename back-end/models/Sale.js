b082f22b978f9fc283a358fbfe1f8bab929ef903
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-11-10 17:14:41 -0300

    tabela sales criada

---
+++ back-end/models/Sale.js
@@ -0,0 +1,19 @@
+const createdSale = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('sale', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    user_id: { type: DataTypes.INTEGER, foreignKeyKey: true },
+    total_price: DataTypes.DECIMAL(9, 2),
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING,
+  }, { timestamps: false });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.user, { foreignKey: 'user_id', as: 'user' });
+  };
+
+  return Sale;
+};
+
+module.exports = createdSale;
