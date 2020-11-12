13b1e8a2c34919e0bb738acbb41414cd5bef4552
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-11-12 16:41:00 -0300

    chat working

--- front-end/src/pages/chat/ChatPage.jsx
+++ front-end/src/pages/chat/ChatPage.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
+import MenuTop from '../../components/menuTop/MenuTop';
+import Sidebar from '../../components/sidebar/Sidebar';
 
 const io = window.io;
 
@@ -6,10 +8,13 @@ function ChatPage() {
   const socket = useRef();
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState('');
+  const [userEmail, setUserEmail] = useState('');
 
   useEffect(() => {
+    const { email } = JSON.parse(window.localStorage.getItem('user'));
+    setUserEmail(email);
     socket.current = io('http://localhost:3001');
-    socket.current.emit('private-history', { id: 'militaorodrigo9@gmail.com' });
+    socket.current.emit('private-history', { id: email });
   }, []);
 
   useEffect(() => {
@@ -24,46 +29,50 @@ function ChatPage() {
     e.preventDefault();
     const messageObj = {
       chatMessage: messageInput,
-      nickname: 'militaorodrigo9@gmail.com',
+      nickname: userEmail,
       to: 'store',
     };
     socket.current.emit('private', messageObj);
-    socket.current.emit('private-history', { id: 'militaorodrigo9@gmail.com' });
+    socket.current.emit('private-history', { id: userEmail });
 
     setMessageInput('');
   }
 
 
   return (
-    <div className="container">
-      <fieldset>
-        <span>Mensagens</span>
-        <ul>
-          {messages.map(({chatMessage, from, timestamp}, index) => (
-            <li key={`${index}`}>
-              <span data-testid="nickname">{from}</span> - 
-              <span data-testid="message-time">{timestamp}</span> <br/> 
-              <span data-testid="text-message">{chatMessage}</span>
-            </li>
-          ))}
-        </ul>
-      </fieldset>
-      <input
-        className="form-control"
-        type="text"
-        data-testid="message-input"
-        value={messageInput}
-        onChange={(e) => setMessageInput(e.target.value)}
-        id="message-box"
-      />
-      <button
-        onClick={(e) => handleSendMessage(e)}
-        className="btn btn-success"
-        type="submit"
-        id="send-button"
-        data-testid="send-message">
-          Enviar
-      </button>
+    <div>
+      <MenuTop />
+      <div className="container mt-5">
+      <Sidebar />
+        <fieldset>
+          <span>Mensagens</span>
+          <ul>
+            {messages.map(({chatMessage, from, timestamp}, index) => (
+              <li key={`${index}`}>
+                <span data-testid="nickname">{from}</span> - 
+                <span data-testid="message-time">{timestamp}</span> <br/> 
+                <span data-testid="text-message">{chatMessage}</span>
+              </li>
+            ))}
+          </ul>
+        </fieldset>
+        <input
+          className="form-control"
+          type="text"
+          data-testid="message-input"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+          id="message-box"
+        />
+        <button
+          onClick={(e) => handleSendMessage(e)}
+          className="btn btn-success"
+          type="submit"
+          id="send-button"
+          data-testid="send-message">
+            Enviar
+        </button>
+      </div>
     </div>
   );
 }
