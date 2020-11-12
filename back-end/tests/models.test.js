52fcbe5517817f05e5829f4c451afdbe7986f33e
William Igor <williamigorsilva@gmail.com>
2020-11-12 17:04:17 -0300

    verify tests

---
+++ back-end/tests/models.test.js
@@ -0,0 +1,8 @@
+const { getAllProducts } = require('../services/products');
+
+describe('Test get products', () => {
+  test('Should return products', () => {
+    // getAllProducts = jest.fn()
+    expect(2).toBe(2);
+  });
+});
