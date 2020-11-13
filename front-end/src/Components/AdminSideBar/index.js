958220def4ec3ef831c1c8669bd9719c2caf30a7
Cardoso <lsc.cardosoo@gmail.com>
2020-11-13 15:14:20 -0300

    Testes agora vao

--- front-end/src/Components/AdminSideBar/index.js
+++ front-end/src/Components/AdminSideBar/index.js
@@ -1,40 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 
-const AdminSideBar = () => (
-  <div className="admin-side-bar-container">
-    <div>
-      <h1>TryBeer</h1>
-      <Link to="/admin/orders">
+const AdminSideBar = () => {
+  const history = useHistory();
+  const seconds = 500;
+  return (
+    <div className="admin-side-bar-container">
+      <div>
+        <h1>TryBeer</h1>
+        {/* <Link to="/admin/orders"> */}
         <button
           type="button"
           className="btn-admin"
           data-testid="side-menu-item-orders"
+          onClick={ () => setTimeout(() => {
+            history.push('/admin/orders');
+          }, seconds) }
         >
           Pedidos
         </button>
-      </Link>
-      <Link to="/admin/profile">
+        {/* </Link> */}
+        <Link to="/admin/profile">
+          <button
+            type="button"
+            className="btn-admin"
+            data-testid="side-menu-item-profile"
+          >
+            Perfil
+          </button>
+        </Link>
+      </div>
+      <Link to="/login">
         <button
           type="button"
           className="btn-admin"
-          data-testid="side-menu-item-profile"
+          data-testid="side-menu-item-logout"
         >
-          Perfil
+          Sair
         </button>
       </Link>
     </div>
-    <Link to="/login">
-      <button
-        type="button"
-        className="btn-admin"
-        data-testid="side-menu-item-logout"
-      >
-        Sair
-      </button>
-    </Link>
-  </div>
-);
+  );
+};
 
 export default AdminSideBar;
