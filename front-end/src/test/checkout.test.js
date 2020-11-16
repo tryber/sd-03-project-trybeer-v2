3ca8b7f63710414dc461350b191dc2cfd4189ee2
Jafet Henrique <jafetguerra6@gmail.com>
2020-11-16 14:57:12 -0300

    bonus

--- front-end/src/test/checkout.test.js
+++ front-end/src/test/checkout.test.js
@@ -47,6 +47,7 @@ describe('testing checkout page', () => {
     expect(productQnt.innerHTML).toBe('2');
     expect(unitPrice).toHaveTextContent('2.20');
     expect(ProductTotalValue).toHaveTextContent('4.40');
+    
     window.location.reload();
     expect(product.innerHTML).toBe('Skol Lata 250ml');
     expect(productQnt.innerHTML).toBe('2');
