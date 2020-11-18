2842facfe8ba29300d7b0ecd3da2a941fb133d35
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-11-17 22:53:42 -0300

    Refatorando o chat de cliente

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
+const socketIo = require('socket.io');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-module.exports = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIo(server);
+
+io.on('connect', (socket) => {
+  console.log(`${socket.id} connected`);
+
+  socket.on('message', ({ messageValue }) => {
+    console.log(`${socket.id} : ${messageValue}`);
+  });
+});
+
+module.exports = server;
