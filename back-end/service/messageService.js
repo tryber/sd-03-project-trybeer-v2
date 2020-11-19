58bf137e1e1879fce43d93124aa0e56cd74dbe6c
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-11-19 02:45:16 -0300

    passando

--- back-end/service/messageService.js
+++ back-end/service/messageService.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const messageModel = require('../models/messageModel');
-=======
 const messageModel = require('../MongoModel/messageModel');
->>>>>>> 6c229c450a01f84ea281204fac84a4f591d80d78
 
 const getAllChats = async () => {
   const chat = await messageModel.getAllChats();
