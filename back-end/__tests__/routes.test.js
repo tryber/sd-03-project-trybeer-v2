bc0c93883c609ddbb48a8665c96fb0bd512a1ee0
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-11-19 18:44:32 -0300

    passando

---
+++ back-end/__tests__/routes.test.js
@@ -0,0 +1,149 @@
+const request = require('supertest');
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const httpFactory = require('../http');
+const routes = require('../routes');
+
+const app = httpFactory(express, routes, cors, bodyParser);
+const connect = require('./connection');
+const { deleteProductSales, deleteUser } = require('./helpers/functions');
+
+afterEach(async () => {
+  await connect();
+});
+describe('Testando Login', () => {
+  it('Testando o login correto', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+        email: 'bruno.batista@gmail.com',
+        password: '12345678',
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+  it('Testando o login incorreto', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+        email: 'bruno.batista@gmail.com',
+        password: '1234',
+      });
+    expect(res.statusCode).toEqual(401);
+  });
+  it('Testando o login sem email', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+        password: '1234',
+      });
+    expect(res.statusCode).toEqual(500);
+  });
+});
+
+describe('Testando produtos indiviudais', () => {
+  test('Testando a pesquisa por produtos indiviudais', async () => {
+    const res = await request(app)
+      .get('/individualProduct');
+    expect(res.statusCode).toEqual(200);
+  });
+  test('Testando a inserção de produtos para uma venda', async () => {
+    deleteProductSales();
+    const res = await request(app)
+      .post('/individualProduct')
+      .send({
+        saleId: 1, productId: 7, quantity: 2,
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+  test('Testando erro na inserção de produtos para uma venda', async () => {
+    const res = await request(app)
+      .post('/individualProduct')
+      .send({
+        saleId: 1, productId: 7, quantity: 2,
+      });
+    expect(res.statusCode).toEqual(500);
+  });
+});
+
+describe('Testando produtos', () => {
+  test('Testando o retorno da lista de produtos', async () => {
+    const res = await request(app)
+      .get('/products');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe('Testando Perfil', () => {
+  test('Testando o retorno das informações do Perfil', async () => {
+    const res = await request(app)
+      .put('/profile')
+      .send({ name: 'Bruno Silva Batista',
+        email: 'bruno.batista@gmail.com',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJuYW1lIjoiQnJ1bm8gU2lsdmEgQmF0aXN0YSIsImVtYWlsIjoiYnJ1bm8uYmF0aXN0YUBnbWFpbC5jb20iLCJyb2xlIjoiY2xpZW50In0sImlhdCI6MTYwMTg0MDE1MywiZXhwIjoxNjAzNTY4MTUzfQ.S0TH3VFOKbowDDgeZJSHhYPZKbW0Giu1ovRIiuru_Rs',
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+  test('Testando erro no  retorno das informações do Perfil sem token', async () => {
+    const res = await request(app)
+      .put('/profile')
+      .send({ name: 'Bruno Silva Batista',
+        email: 'bruno.batista@gmail.com',
+        token: '',
+      });
+    expect(res.statusCode).toEqual(401);
+  });
+  test('Testando erro no  retorno das informações do Perfil sem o Email', async () => {
+    const res = await request(app)
+      .put('/profile')
+      .send({ name: 'Bruno Silva Batista',
+        email: '',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJuYW1lIjoiQnJ1bm8gU2lsdmEgQmF0aXN0YSIsImVtYWlsIjoiYnJ1bm8uYmF0aXN0YUBnbWFpbC5jb20iLCJyb2xlIjoiY2xpZW50In0sImlhdCI6MTYwMTg0MDE1MywiZXhwIjoxNjAzNTY4MTUzfQ.S0TH3VFOKbowDDgeZJSHhYPZKbW0Giu1ovRIiuru_Rs',
+      });
+    expect(res.statusCode).toEqual(500);
+  });
+});
+
+describe('Testando o Registro', () => {
+  test('Registrando um usuário', async () => {
+    deleteUser();
+    const res = await request(app)
+      .post('/register')
+      .send({ name: 'clayton pereira',
+        email: 'cpereiramt@gmail.com',
+        password: '12345678',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJuYW1lIjoiQnJ1bm8gU2lsdmEgQmF0aXN0YSIsImVtYWlsIjoiYnJ1bm8uYmF0aXN0YUBnbWFpbC5jb20iLCJyb2xlIjoiY2xpZW50In0sImlhdCI6MTYwMTg0MDE1MywiZXhwIjoxNjAzNTY4MTUzfQ.S0TH3VFOKbowDDgeZJSHhYPZKbW0Giu1ovRIiuru_Rs',
+        role: 'comum',
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe('Testando as vendas', () => {
+  test('Testando a pesquisa de vendas', async () => {
+    const res = await request(app)
+      .get('/sales');
+    expect(res.status).toEqual(200);
+  });
+  test('Testando a inserção de uma venda', async () => {
+    const res = await request(app)
+      .post('/sales')
+      .send({
+        email: 'bruno.batista@gmail.com',
+        total: '5',
+        address: 'rua 16',
+        number: '2',
+        date: '2020/05/01',
+      });
+    expect(res.status).toEqual(200);
+  });
+  test('Testando a alteração do status da venda', async () => {
+    const res = await request(app)
+      .put('/sales')
+      .send({
+        id: '1',
+      });
+    expect(res.status).toEqual(200);
+  });
+});
