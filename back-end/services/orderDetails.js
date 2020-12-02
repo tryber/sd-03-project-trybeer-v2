47b664ea21eb0ebb0ac1743c8658c84dcad3bc83
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-12-01 22:16:46 -0300

    backend de registro de vendas funcionando corretamente

--- back-end/services/orderDetails.js
+++ back-end/services/orderDetails.js
@@ -1,21 +1,20 @@
+const { Op } = require('sequelize');
 const { salesProducts, products, sales, users } = require('../models');
 
 const getDetails = async (orderNumber, userEmail) => {
-  let status;
-
   const orderDetails = await salesProducts.findAll({ where: { sale_id: orderNumber } });
   const findId = await users.findAll({ where: { email: userEmail } });
   const { id } = findId[0];
+  console.log(orderNumber);
   console.log(id);
 
-  await sales.findAll({ where: { user_id: id } }).then((result) => result.map((e) => {
-    status = e.dataValues;
-    return status;
-  }));
+  const saleInfo = await sales.findAll(
+    { where: { [Op.and]: [{ user_id: id }, { id: orderNumber }] } },
+  );
 
   const details = orderDetails.map(async ({ quantity, product_id: productId }) => {
     const { name, price } = await products.findByPk(productId);
-    return { quantity, name, price, status };
+    return { quantity, name, price, saleInfo };
   });
   const promise = await Promise.all(details);
   return promise;
