7801b5a1f0ced1a2f55708a86f60c36387c7e438
gabryelryba <gabryel.ryba@gmail.com>
2020-12-03 13:05:40 -0300

    ChatComponent criado e separado da ClientChatPage

--- src/pages/Client/ClientChat/ClientChatPage.jsx
+++ src/pages/Client/ClientChat/ClientChatPage.jsx
@@ -1,98 +1,18 @@
-import React, {
-  useState, useEffect, useRef, useCallback,
-} from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { io } from 'socket.io-client';
 import ClientNavBar from '../../../components/Client/ClientNavBar/ClientNavBar';
-
-const getMessageHistory = async (userEmail) => {
-  const messages = await fetch('http://localhost:3001/chat/history', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userEmail }),
-  });
-  const res = await messages.json();
-  return res;
-};
+import ChatComponent from '../../../components/Chat/ChatComponent';
+import { getMessageHistory } from '../../../services';
 
 function ClientChatPage() {
   const user = JSON.parse(localStorage.getItem('user') || '{}');
-  const { userEmail } = user;
-  const time = new Date().toLocaleTimeString('pt-br');
-  const socket = useRef();
 
-  const sliceInit = 0;
-  const sliceEnd = 2;
-
-  const [message, setMessage] = useState('');
-  const [messageHistory, setMessageHistory] = useState();
-  const [refreshMessages, setRefreshMessages] = useState(false);
-
-  const fetchMessageHistory = useCallback(
-    async () => setMessageHistory(await getMessageHistory(userEmail)), [userEmail],
-  );
-
-  useEffect(() => {
-    socket.current = io('http://localhost:3001', { transports: ['websocket'] });
-    fetchMessageHistory();
-  }, [userEmail, fetchMessageHistory, refreshMessages]);
-
-  if (!userEmail) return <Redirect to="/login" />;
+  if (!user) return <Redirect to="/login" />;
 
   return (
     <div style={ { background: 'white' } }>
       <ClientNavBar title="Chat" />
-      <div
-        style={ {
-          display: 'flex', flexDirection: 'column', width: '360px', height: '520px', overflowY: 'scroll',
-        } }
-      >
-        <ul>
-          {messageHistory && messageHistory.history
-            ? messageHistory.history.sent.map((item) => (
-              <li style={ { textAlign: 'left', padding: '0', margin: '0' } } key={ item.timestamp }>
-                <p data-testid="nickname" style={ { padding: '0', margin: '0' } }>
-                  {userEmail}
-                </p>
-                <div
-                  style={ {
-                    display: 'flex', border: '1px solid black', width: '250px', padding: '15px', borderRadius: '15px', background: '#00af9c', borderTopLeftRadius: '0',
-                  } }
-                >
-                  <p data-testid="message-time">
-                    {item.timestamp.split(':').slice(sliceInit, sliceEnd).join(':')}
-
-                  </p>
-                  <p data-testid="text-message" style={ { marginLeft: '20px' } }>
-                    {item.chatMessage}
-                  </p>
-                </div>
-              </li>)) : 'Loading messages...'}
-        </ul>
-      </div>
-      <div
-        style={ {
-          marginTop: '10px', width: '360px', background: 'red', height: '37px', paddingTop: '5px',
-        } }
-      >
-        <input placeholder="Digite sua mensagem" type="text" data-testid="message-input" onChange={ (e) => setMessage(e.target.value) } value={ message } style={ { width: '250px', height: '25px' } } />
-        <button
-          style={ { width: '70px', height: '31px', marginLeft: '4px' } }
-          type="button"
-          data-testid="send-message"
-          onClick={ () => {
-            socket.current.emit('message', { message, userEmail, time });
-            setRefreshMessages(!refreshMessages);
-            setMessage('');
-          } }
-        >
-          Enviar
-        </button>
-      </div>
-
+      <ChatComponent callback={ getMessageHistory } />
     </div>
   );
 }
