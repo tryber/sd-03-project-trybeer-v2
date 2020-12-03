da17ebe4c5560af88d9d80410b408ed33dde324a
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-12-03 18:27:06 -0300

    teste de login e profile

--- back-end/services/login.js
+++ back-end/services/login.js
@@ -14,8 +14,9 @@ const validateLogin = async (email, pass) => {
   if (pass.length < 6) return { code: 400, message: 'Senha no formato incorreto' };
   const userInfo = (await users.findAll({ where: { email } }))[0];
   if (!userInfo) return { code: 404, message: 'usuário não encontrado' };
-  if (userInfo.password !== pass) return { code: 401, message: 'senha incorreta' };
-  const { password, ...info } = userInfo.dataValues;
+  console.log('passa', userInfo, userInfo.password, pass);
+  if (userInfo.password !== pass && userInfo.email) return { code: 401, message: 'senha incorreta' };
+  const { password, ...info } = userInfo;
   return info;
 };
 
